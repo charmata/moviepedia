f1f28c248af70805742ed4d7a592f427e224dcdb
Chris Armata <chris.armata@gmail.com>
2019-05-25 11:35:34 -0400

    Associate user model with review and watchlist models

--- models/user.js
+++ models/user.js
@@ -2,5 +2,17 @@ module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define("User", {
     name: DataTypes.STRING
   });
+
+  User.associate = models => {
+    // Delete all reviews made by user when they are deleted
+    User.hasMany(models.Review, {
+      onDelete: "cascade"
+    });
+    // Delete all movies added to the user's watchlist when they are deleted
+    User.hasMany(models.Watchlist, {
+      onDelete: "cascade"
+    });
+  };
+
   return User;
 };
