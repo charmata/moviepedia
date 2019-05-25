13df2a89f60ab6369946cf6478fe435ae559de0c
Chris Armata <chris.armata@gmail.com>
2019-05-25 12:18:16 -0400

    Add watchlist associations

--- models/watchlist.js
+++ models/watchlist.js
@@ -1,7 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-  var Watchlist = sequelize.define("Watchlist", {
-    movie_id: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER
-  });
+  var Watchlist = sequelize.define("Watchlist");
+
+  Watchlist.associate = models => {
+    Watchlist.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    Watchlist.belongsTo(models.Movie, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
   return Watchlist;
 };
