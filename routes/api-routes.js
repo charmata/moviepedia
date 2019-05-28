fbcaad503fb3f61dcd7a23278fa821c16824e708
Chris Armata <chris.armata@gmail.com>
2019-05-28 18:42:53 -0400

    Add user routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 var db = require("../models");
 
 module.exports = app => {
+  // Movie
   app.get("/api/movie", (req, res) => {
     db.Movie.findAll({})
       .then(data => {
@@ -25,6 +26,29 @@ module.exports = app => {
   });
   app.post("/api/movie", (req, res) => {
     db.Movie.create(req.body)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  // User
+  app.get("/api/user/:id", (req, res) => {
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  app.post("/api/user", (req, res) => {
+    db.User.create(req.body)
       .then(data => {
         res.json(data);
       })
