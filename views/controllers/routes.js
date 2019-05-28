0bf8b76a27b48891fc70b84b9cbf533c8f9292c9
zain murtaza <zainmurtaza315@gmail.com>
2019-05-28 19:30:07 -0400

    added axios into routes

--- views/controllers/routes.js
+++ views/controllers/routes.js
@@ -19,11 +19,21 @@ for (var i = 2; i < nodeArgs.length; i++) {
 }
 
 // Axios request finding the API and movie searched
-var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&apikey=26fbc7b2";
 
+var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&apikey=26fbc7b2";
+var queryURL2 =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" +
+  movieName +
+  "&order=rating&type=video&videoDefinition=high&videoEmbeddable=true&key=AIzaSyAoVT5ZZwk6f0aT-ClGOaf3eoGnC2zRKko";
+console.log(queryURL2);
+console.log(queryURL2.items);
 // Attempting to debug URL
 console.log(queryURL);
 
 axios.get(queryURL).then(function(response) {
-  console.log(response);
+  console.log(response.data.items);
+});
+
+axios.get(queryURL2).then(function(response2) {
+  console.log(response2.data.items);
 });
