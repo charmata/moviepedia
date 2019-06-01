dab76b3d3ee0123441174e636dee3f97ed6bde79
Chris Armata <chris.armata@gmail.com>
2019-06-01 02:46:26 -0400

    Refactor movie search
    - Get results from server endpoint
    
    - Redesign search to display multiple results

--- public/app.js
+++ public/app.js
@@ -45,24 +45,53 @@ var logout = () => {
   sessionStorage.removeItem("name");
 };
 
-$(document).ready(function() {
-  $("#submitBtn").click(function() {
-    var results = $("#searchText")
+var movieSearch = () => {
+  if ($("#searchText")) {
+    $("#searchResults").empty();
+    var movieName = $("#searchText")
       .val()
       .trim();
-    var queryURL = "http://www.omdbapi.com/?t=" + results + "&apikey=26fbc7b2";
     $.ajax({
-      url: queryURL,
+      url: `/api/omdb?q=${movieName}`,
       method: "GET"
-    }).then(function(response) {
-      $("#results").text(response.Title);
-      $("#plot").text(response.Plot);
-      $("#image").attr("src", response.Poster);
-      $("#rating").text(response.Ratings[1].Value);
-      console.log(response);
+    })
+      .then(response => {
+        if (response) {
+          response.Search.forEach(movie => {
+            var card = $("<div>")
+              .addClass("col-2 card mb-3")
+              .attr("data-id", movie.imdbID);
+            var poster = $("<img>")
+              .addClass("card-img-top")
+              .attr("src", movie.Poster);
+            var body = $("<div>").addClass("card-body");
+            var title = $("<h5>")
+              .addClass("card-title")
+              .text(movie.Title);
 
-      //   $("#results").append(img);
-    });
+            body.append(title);
+            card.append(poster, body);
+            $("#searchResults").append(card);
+
+            /*<div class="card-body">
+              <h5 class="card-title" id="results" />
+              <p class="card-text" id="plot" />
+              <p id="rating" />
+            </div>*/
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        $("#searchText").val("");
+      });
+  }
+};
+
+$(document).ready(function() {
+  $(document).on("click", "#submitBtn", e => {
+    e.preventDefault();
+    movieSearch();
   });
 
   $(document).on("click", "#login-submit", e => {
@@ -77,38 +106,4 @@ $(document).ready(function() {
     e.preventDefault();
     logout();
   });
-  // entering movie name
-
-  //   var movieName = "";
-
-  //Loop through all the words in the node argument, plus taking into account any "+" added into movie descriptions
-
-  //   for (var i = 2; i < nodeArgs.length; i++) {
-  //     if (i > 2 && i < nodeArgs.length) {
-  //       movieName = movieName + "+" + nodeArgs[i];
-  //     } else {
-  //   //       movieName += nodeArgs[i];
-  //   //     }
-  //   // }
-
-  //   // Axios request finding the API and movie searched
-
-  //   var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&apikey=26fbc7b2";
-  //   var queryURL2 =
-  //     "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" +
-  //     movieName +
-  //     "&order=rating&type=video&videoDefinition=high&videoEmbeddable=true&key=AIzaSyAoVT5ZZwk6f0aT-ClGOaf3eoGnC2zRKko";
-  //   console.log(queryURL2);
-  //   console.log(queryURL2.items);
-  //   // Attempting to debug URL
-  //   console.log(queryURL);
-
-  //   axios.get(queryURL).then(function(response) {
-  //     console.log(response.data.items);
-  //   });
-
-  //   axios.get(queryURL2).then(function(response2) {
-  //     console.log(response2.data.items);
-  //   });
-  // })
 });
